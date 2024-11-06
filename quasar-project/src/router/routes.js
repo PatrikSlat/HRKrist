cf5de58ecbc128edf840e84ad736799d895bda74
PatrikSlat <patrik.slat@gmail.com>
2024-11-06 11:14:51 +0100

    Created routes for new pages

--- quasar-project/src/router/routes.js
+++ quasar-project/src/router/routes.js
@@ -1,18 +1,23 @@
 const routes = [
-  {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
-  },
-
-  // Always leave this as last one,
-  // but you can also remove it
-  {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
-
-export default routes
+    {
+      path: '/',
+      component: () => import('layouts/MainLayout.vue'),
+      children: [
+        { path: '', component: () => import('pages/IndexPage.vue') },
+        { path: '/EventsPage', component: () => import('pages/EventsPage.vue') },
+        { path: '/ProfilPage', component: () => import('pages/ProfilPage.vue') },
+        { path: '/HomePage', component: () => import('pages/HomePage.vue') },
+        { path: '/QutesPage', component: () => import('pages/QutesPage.vue') }
+      ]
+    },
+  
+    // Always leave this as last one,
+    // but you can also remove it
+    {
+      path: '/:catchAll(.*)*',
+      component: () => import('pages/ErrorNotFound.vue')
+    }
+  ]
+  
+  export default routes
+  
