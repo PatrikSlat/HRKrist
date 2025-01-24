e1d67d211b1ef36c2995c16673637429efb945df
PatrikSlat <patrik.slat@gmail.com>
2025-01-24 22:54:40 +0100

    Created backend route for sending email using nodemailer

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 const mysql = require('mysql');
+const nodemailer = require("nodemailer")
 
 const connection = mysql.createConnection({
   host     : 'ucka.veleri.hr',
@@ -36,6 +37,35 @@ app.get("/api/random/test", async (req, res) => {
         res.status(500).json({ message: "Error while Fetching !!!" });
     }
 });
+
+//////// EMAIL.JS CONFIGURATION 
+app.post("/api/send-email", async (req, res) => {
+    const { email, message } = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: "hrkrist.help@gmail.com",
+            pass: "fbis pfqn pnrr yiur", 
+        },
+    });
+
+    const mailOptions = {
+        from: "hrkrist.help@gmail.com",
+        to: "hrkrist.help@gmail.com",
+        subject: "HRKrist help page",
+        text: "user: " + email + "\n" + "poruka: " + message,
+    };
+
+    try {
+        await transporter.sendMail(mailOptions);
+        res.json({ message: "Email sent successfully!" });
+    } catch (error) {
+        console.error("Email sending failed", error);
+        res.status(500).json({ message: "Email sending failed" });
+    }
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server listening at http://localhost:${PORT}`);
